e99a93252e48b427acef774b1fd25bed06ddc3af
Pavel-Bylkov <oldman.bpv@gmail.com>
2023-07-03 21:08:01 +0700

    update

--- app/src/main/java/com/example/samsungfinal/SearchActivity.java
+++ app/src/main/java/com/example/samsungfinal/SearchActivity.java
@@ -5,21 +5,103 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.app.DatePickerDialog;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+    String city;
+    Calendar dateAndTime = Calendar.getInstance();
+    String date_start;
+    String date_end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // Выпадающий список городов
+        setSpinnerView();
+
+        // Выбор даты
+        setInitialDateTime();
+
+//        https://kudago.com/public-api/v1.2/events/?location=spb&actual_since=1384252440&actual_until=1384292440&is_free=1&categories=exhibition,concert
+        // https://kudago.com/public-api/v1.4/events/?lang=ru&fields=&expand=&order_by=&text_format=&ids=&location=nsk&actual_since=1444385206&actual_until=1444385405&is_free=&categories=&lon=&lat=&radius=
+    }
+
+    // отображаем диалоговое окно для выбора даты
+    public void setDate(View v) {
+        new DatePickerDialog(SearchActivity.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+    // установка начальных даты и времени
+    private void setInitialDateTime() {
+
+        date_start = DateUtils.formatDateTime(this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME);
+    }
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
+
+    private void setSpinnerView() {
+        HashMap<String, String> locations = new HashMap<>();
+        locations.put("Екатеринбург", "ekb");
+        locations.put("Казань", "kzn");
+        locations.put("Москва", "msk");
+        locations.put("Нижний Новгород", "nnv");
+        locations.put("Новосибирск", "nsk");
+        locations.put("Санкт-Петербург", "spb");
+        List<String> cities = new ArrayList<String>(locations.keySet());
+        Spinner spinner = findViewById(R.id.spinner_city);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, cities);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Получаем выбранный город
+                String item = (String) parent.getItemAtPosition(position);
+                city = locations.get(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        };
+        spinner.setOnItemSelectedListener(itemSelectedListener);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         String main = getResources().getString(R.string.home);
-        menu.add(0,0,0,main);
+        menu.add(0, 0, 0, main);
         return super.onCreateOptionsMenu(menu);
     }
 
