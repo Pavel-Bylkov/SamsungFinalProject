ac8e12d8c9930cf33c373162b00d029b2d950227
Pavel-Bylkov <oldman.bpv@gmail.com>
2023-07-06 17:37:54 +0700

    update

--- app/src/main/java/com/example/samsungfinal/EventDetail.java
+++ app/src/main/java/com/example/samsungfinal/EventDetail.java
@@ -9,19 +9,19 @@ public class EventDetail implements Serializable {
     @SerializedName("id")
     public int id; //- идентификатор
     @SerializedName("publication_date")
-    public String publication_date; //- дата публикации
+    public long publication_date; //- дата публикации
     @SerializedName("dates")
-    public String dates; //- даты проведения
+    public List<Date> dates; //- даты проведения
     @SerializedName("title")
     public String title; //- название
     @SerializedName("slug")
     public String slug; //- слаг
     @SerializedName("place")
-    public String place; //- место проведения
+    public Place place; //- место проведения
     @SerializedName("description")
     public String description; //- описание
     @SerializedName("location")
-    public String location; //- город проведения
+    public Location location; //- город проведения
     @SerializedName("categories")
     public List<String> categories; //- список категорий
     @SerializedName("age_restriction")
@@ -29,10 +29,51 @@ public class EventDetail implements Serializable {
     @SerializedName("price")
     public String price; //- стоимость
     @SerializedName("images")
-    public String images; //- картинки
+    public List<Image> images; //- картинки
     @SerializedName("site_url")
     public String site_url; //- адрес события на сайте kudago.com
     @SerializedName("tags")
     public List<String> tags; //- тэги события
 
+    class Date implements Serializable {
+        @SerializedName("start_date")
+        public String start_date;
+        @SerializedName("start_time")
+        public String start_time;
+        @SerializedName("start")
+        public long start;
+        @SerializedName("end_date")
+        public String end_date;
+        @SerializedName("end_time")
+        public String end_time;
+        @SerializedName("end")
+        public long end;
+    }
+
+    class Place implements Serializable {
+        @SerializedName("id")
+        public int id; //- идентификатор
+        @SerializedName("title")
+        public String title;
+        @SerializedName("address")
+        public String address;
+        @SerializedName("phone")
+        public String phone;
+        @SerializedName("site_url")
+        public String site_url;
+    }
+
+    class Location implements Serializable {
+        @SerializedName("slug")
+        public String slug;
+        @SerializedName("name")
+        public String name;
+    }
+
+    class Image implements Serializable {
+        @SerializedName("image")
+        public String image;
+    }
 }
+
+// id,publication_date,dates,title,slug,place,description,location,categories,age_restriction,price,images,site_url,tags
